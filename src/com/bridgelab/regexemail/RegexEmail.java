07795b0f504ab50ffd28e3156866df911a6994b2
mukeshkapse <mukeshkapse1832@gmail.com>
2022-09-14 12:59:04 +0530

    Added: Valid Email

--- src/com/bridgelab/regexemail/RegexEmail.java
+++ src/com/bridgelab/regexemail/RegexEmail.java
@@ -5,8 +5,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexEmail {
- static Pattern pattern = Pattern.compile("^[A-Z]{3,}");
+
+    static Pattern pattern = Pattern.compile("^[A-Z]{3,}");
+
     static Pattern pattern2 = Pattern.compile("^[A-Z]{3,}");
+    static Pattern ForEmail = Pattern.compile(("^[a-z]{3,}[.]+[a-z]{3,}?[@]+[a-z]{2,}[.]+[a-z]{2,}[.]+[a-z]{2}$"));
+
     public static void main(String[] args) {
 
         Scanner Sc = new Scanner(System.in);
@@ -14,12 +18,18 @@ public class RegexEmail {
         Matcher matcher = pattern.matcher(Sc.nextLine());
         System.out.println("Enter Last Name");
         Matcher matcher1 = pattern2.matcher(Sc.nextLine());
+        System.out.println("Enter Email");
+        Matcher matcher3 = ForEmail.matcher(Sc.nextLine());
+
 
         if(matcher.matches()&& matcher1.matches()){
             System.out.println("Valid");
         }
+        if(matcher3.matches()){
+            System.out.println("Valid Email");
+        }
         else{
-            System.out.println("Invalid");
+            System.out.println("Invalid Detail");
         }
     }
 }
